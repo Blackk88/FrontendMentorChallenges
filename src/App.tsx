07a7c518a6305aeec07b2697e8fac9fbb3844c21
Aleksandr Trofimov <trofimovalexandr88@gmail.com>
2023-04-05 20:12:31 +0200

    add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ArrowIcon from './assets/icon-arrow.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="main-container">
+      <form action="" className="form">
+        <div className="form__input-wrapper">
+          <div className="form__input-group">
+            <label htmlFor="date" className="form__label">
+              Day
+            </label>
+            <input
+              type="text"
+              name="date"
+              id="date"
+              className="form__input"
+              placeholder="DD"
+              size={3}
+              maxLength={2}
+            />
+          </div>
+          <div className="form__input-group">
+            <label htmlFor="month" className="form__label">
+              Month
+            </label>
+            <input
+              type="text"
+              name="month"
+              id="month"
+              className="form__input"
+              placeholder="MM"
+              size={3}
+              maxLength={2}
+            />
+          </div>
+          <div className="form__input-group">
+            <label htmlFor="year" className="form__label">
+              Year
+            </label>
+            <input
+              type="text"
+              name="year"
+              id="year"
+              className="form__input"
+              placeholder="YYYY"
+              size={5}
+              maxLength={4}
+            />
+          </div>
+        </div>
+        <button type="submit" className="form__submit">
+          <img
+            src={ArrowIcon}
+            alt="Down Arrow"
+            className="form__submit--icon"
+          />
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        <div className="break-line"></div>
+      </form>
+
+      <p className="text__output">
+        <span className="text--purple">38</span> years
+      </p>
+      <p className="text__output">
+        <span className="text--purple">3</span> months
+      </p>
+      <p className="text__output">
+        <span className="text--purple">26</span> days
       </p>
     </div>
   )
